03618d01d119aa0c34cc0923b9f7390d1978248c
Dawit212119 <dawit.workye-ug@aau.edu.et>
2024-10-20 01:43:17 +0300

    update server

--- api/index.js
+++ api/index.js
@@ -37,9 +37,9 @@ app.listen(port, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 //app.use("/api/listing", listingRouter);
 //app.use("/api/user", userRouter);

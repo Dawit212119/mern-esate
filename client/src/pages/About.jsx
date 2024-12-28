4afa68ecf7b2ecf80349460e75288cc6326a9c4e
Dawit212119 <dawit.workye-ug@aau.edu.et>
2024-12-28 14:45:11 +0300

    update

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 export default function About() {
   return (
     <div className="w-full mx-auto max-w-6xl py-20 px-3">
-      <h1 className="text-3xl font-bold mb-3">About Mern Estate</h1>
+      <h1 className="text-3xl font-bold mb-3">About Real Estate</h1>
       <p className="mb-4">
-        Sahand Estate is a leading real estate agency that specializes in
-        helping clients buy, sell, and rent properties in the most desirable
+        Real Estate is a leading real estate agency that specializes in helping
+        clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
         as possible.
